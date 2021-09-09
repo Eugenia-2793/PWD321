33a274608ab2c5509bf654ae1a5303d0b74727fa
eugeniagarcia <eugegarciaruiz2@gmail.com>
2021-09-09 15:36:14 -0300

    un caos la vista

--- Vista/estructura/cabecera.php
+++ Vista/estructura/cabecera.php
@@ -1,13 +1,44 @@
 <html>
-<title><?PHP $Titulo?></title>
-<header>
+  <head>
+        <!-- titulo -->
+    <title><?PHP $Titulo?></title>
+
 
 <!--Incluyo bootstrap y estilo personalizado--->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 <link href="../estilos/es.css" type="text/css" rel="stylesheet" />
-</header>
+
+ 
+         <!-- CSS & JS -->
+        <link rel="stylesheet" type="text/css" href="../../../css//bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="../../../CSS/Boostrap v.4.5.2/bootstrap.min.css">
+
+
+        <link rel="stylesheet" type="text/css" href="../../../CSS/Boostrap v.4.5.2/bootstrapValidator.min.css">
+        <!-- icons -->
+        <link rel="stylesheet" type="text/css" href="../../../CSS/Icons/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+        <!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"> -->
+        <script src="https://kit.fontawesome.com/c3fbd0a6e3.js" crossorigin="anonymous"></script>
+
+
+
+
 </head>
 <body class="bg-light">
+
+
+<!----
+    
+            include_once("../../../../configuracion.php");
+            include_once("../../../Estructura/lateral.php");
+ 
+---->
+
+
+
+
+
+
 
 <!--------------------------------DISEÑO CABECERA----------------------------------------------->
 
