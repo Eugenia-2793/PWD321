9371cbaa3b4dd729629a9fc527d55ab0fcfe04d5
eugeniagarcia <eugegarciaruiz2@gmail.com>
2021-09-09 14:44:50 -0300

    ordene toda la vista

--- configuracion.php
+++ configuracion.php
@@ -2,7 +2,7 @@
 
 $GLOBALS['ROOT'] =$_SERVER['DOCUMENT_ROOT'] ."/PWD321/";
 
-include_once("../../utiles/funciones.php");
-
+//include_once("../../utiles/funciones.php");
+include_once("../../../../Utiles/funciones.php");
 
 ?>
